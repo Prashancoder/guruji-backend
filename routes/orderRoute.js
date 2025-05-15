df2968093ff88582f49a0b67a94052a3c70d40a2
Completrix <aicmt88@gmail.com>
2025-05-15 19:00:27 +0530

    chan

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -13,9 +13,9 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 
-router.route("/order/new").post(newOrder);
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
-router.route("/order/:id").get(getSingleOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
