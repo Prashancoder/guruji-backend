7beffe9ec1765b16b0a3e0aaa9efed228f147568
prashancoder <ps985094@gmail.com>
2025-06-23 19:52:29 +0530

    n

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ connectDatabase();
 
 // ✅ Enable CORS for frontend
 app.use(cors({
-  origin: "https://guruji-frontend.vercel.app",   
+  origin: "http://localhost:3000",
   credentials: true,      // <- Important if you are sending cookies or auth headers
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
